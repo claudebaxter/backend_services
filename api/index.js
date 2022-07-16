a5cab40bb96f24b7442dc7df2d960aac8a221495
elborracho420 <e.baxter@dark-coin.io>
2022-07-16 00:23:52 -0500

    First pass at API service package. Needs error handling, e.g. should send 404s when we request routes not present.

---
+++ api/index.js
@@ -0,0 +1,7 @@
+require('dotenv').config()
+
+const app = require('./app.js');
+
+app.listen(process.env.API_PORT);
+console.log("API is now listening at http://localhost:" +
+    process.env.API_PORT)
